992997dff8f32df39c2bd2c67410b7b2b52004ee
DenisMukonin <dezmukonin16@gmail.com>
2021-01-17 23:29:39 +0300

    перенес select в компонент

---
+++ src/AppTypeBlock.vue
@@ -0,0 +1,32 @@
+<template>
+  <label for="type">Тип блока</label>
+  <select id="type" v-model="valueBlock" @change="$emit('blockType', valueBlock)">
+    <option selected value="title">Заголовок</option>
+    <option value="subtitle">Подзаголовок</option>
+    <option value="avatar">Аватар</option>
+    <option value="text">Текст</option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valueBlock: this.typeBlock
+    }
+  },
+  emits: {
+    'blockType' : null
+  },
+  props: {
+    typeBlock: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
